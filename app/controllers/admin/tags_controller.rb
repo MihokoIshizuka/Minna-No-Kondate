e49aafc1149b8ea7e6e1c7b6d5ee5cbe8962d992
MihokoIshizuka <mi.cobito.1013otter@gmail.com>
2022-06-07 14:58:10 +0000

    [add]tag

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,23 +1,45 @@
 class Admin::TagsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
-  
+    @tags = Tag.all
+    @tag = Tag.new
   end
-  
+
   def create
-  
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to admin_tags_path
+    else
+      render 'index'
+    end
   end
-  
-  def update
-  
-  end
-  
+
   def edit
-  
+    @tag = Tag.find(params[:id])
   end
-  
+
+  def update
+    @tag = Tag.find(params[:id])
+    if @tag.update(tag_params)
+      redirect_to admin_tags_path
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
-  
+    @tag = Tag.find(params[:id])
+    if @tag.destroy
+      redirect_to admin_tags_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 end
