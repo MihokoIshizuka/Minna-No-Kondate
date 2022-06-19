1e1f0bb09c64b3f7cfda81c3704bff217573955b
MihokoIshizuka <mi.cobito.1013otter@gmail.com>
2022-06-19 01:23:00 +0000

    [add]card

---
+++ db/fixtures/development/02_member.rb
@@ -0,0 +1,79 @@
+Member.seed do |s|
+  s.id = 1
+  s.name = 'いしづか'
+  s.email = 'ishizuka@com'
+  s.password = 'ishizuka'
+  s.introduction = 'あんこ中毒'
+end
+
+Member.seed do |s|
+  s.id = 2
+  s.name = '山田花子'
+  s.email = 'yamada@com'
+  s.password = 'yamada'
+  s.introduction = '血圧高めです'
+end
+
+Member.seed do |s|
+  s.id = 3
+  s.name = '田中一郎'
+  s.email = 'tanaka@com'
+  s.password = 'tanaka'
+  s.introduction = 'コレステロール指摘されました。'
+end
+
+Member.seed do |s|
+  s.id = 4
+  s.name = '佐藤二郎'
+  s.email = 'satou@com'
+  s.password = 'satouu'
+  s.introduction = '外食大好き人間です。朝からラーメンすすれます。'
+end
+
+Member.seed do |s|
+  s.id = 5
+  s.name = '加藤よしこ'
+  s.email = 'katou@com'
+  s.password = 'katouu'
+  s.introduction = '管理栄養士してました。'
+end
+
+Member.seed do |s|
+  s.id = 6
+  s.name = '山本みのる'
+  s.email = 'yamamoto@com'
+  s.password = 'yamamoto'
+  s.introduction = '食事管理のために始めました。'
+end
+
+Member.seed do |s|
+  s.id = 7
+  s.name = '鈴木しげお'
+  s.email = 'suzuki@com'
+  s.password = 'suzuki'
+  s.introduction = 'いかにしてワンプレートのごはんを作れるか考えています。'
+end
+
+Member.seed do |s|
+  s.id = 8
+  s.name = '伊藤かずこ'
+  s.email = 'itou@com'
+  s.password = 'itouuu'
+  s.introduction = '最近体重が５キロ増えました。ダイエットします。'
+end
+
+Member.seed do |s|
+  s.id = 9
+  s.name = '吉田清'
+  s.email = 'yoshida@com'
+  s.password = 'yoshida'
+  s.introduction = '一人暮らしでも簡単に作れる料理が知りたい'
+end
+
+Member.seed do |s|
+  s.id = 10
+  s.name = '高橋ようこ'
+  s.email = 'takahashi@com'
+  s.password = 'takahashi'
+  s.introduction = '高橋です。よろしくお願いします。'
+end
