13e1c063a37d568378511c44ec6f334c238e83b7
MihokoIshizuka <mi.cobito.1013otter@gmail.com>
2022-06-23 04:58:02 +0000

    [add]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "011a1c6e7e9e46a9a2ef924cb7a5237e.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
